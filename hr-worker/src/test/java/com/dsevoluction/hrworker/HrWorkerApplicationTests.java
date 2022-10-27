09ec2c5ed2dc0e873878f173bedeec1356bad685
douglas souza <douglas.souza1997@hotmail.com>
2022-10-27 16:59:51 -0300

    created hr-worker project

---
+++ hr-worker/src/test/java/com/dsevoluction/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsevoluction.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
