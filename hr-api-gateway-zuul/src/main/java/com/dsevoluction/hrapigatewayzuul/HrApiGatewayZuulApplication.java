7457a1d6829847f4bfd8fd7726ab2c54ed13a3f7
douglas souza <douglas.souza1997@hotmail.com>
2022-11-10 19:43:11 -0300

    configuring payroll and created project hr-user

--- hr-api-gateway-zuul/src/main/java/com/dsevoluction/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/dsevoluction/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -3,10 +3,8 @@ package com.dsevoluction.hrapigatewayzuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
-@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
