48c346d86b52767e64658782ca90cd8cf224d3aa
douglas souza <douglas.souza1997@hotmail.com>
2022-11-17 20:35:30 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/dsevoluction/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsevoluction.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
