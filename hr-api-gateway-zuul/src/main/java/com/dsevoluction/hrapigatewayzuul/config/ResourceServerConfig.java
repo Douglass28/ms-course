91d17e54431142b71dec21afb253c1db843b7ed1
douglas souza <douglas.souza1997@hotmail.com>
2022-11-25 17:34:39 -0300

    Gateway authorization

---
+++ hr-api-gateway-zuul/src/main/java/com/dsevoluction/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.dsevoluction.hrapigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private JwkTokenStore jwkTokenStore;
+
+    private static final String[] PUBLIC = {"/hr-oauth/oauth/token"};
+    private static final String[] OPERATOR = {"/hr-worker/**"};
+    private static final String[] ADMIN = {"/hr-payroll/**", "/hr-worker/**"};
+
+    // fazendo a leitura de token gerado
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(jwkTokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("ADMIN", "OPERATOR")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
